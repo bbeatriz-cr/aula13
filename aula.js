ec9e613653bd167c9dd403fdff028e2116a51a39
BEATRIZ DA SILVA CRUZ <beatriz.scruz7@senacsp.edu.br>
2024-09-12 17:03:38 -0300

    aula13

---
+++ aula.js
@@ -0,0 +1,19 @@
+function minhaFuncao(){
+    document.querySelector('.minhaDiv').classList.add("classe1","classe2","classe3")
+}
+function minhaFuncaoo(){
+    document.querySelector('.minhaDiv').classList.remove("classe1","classe2","classe3")
+}
+function fundoAzul(){
+    let elemento = document.querySelector("#style")
+    elemento.style.backgroundColor = "blue"
+}
+function fundoTransp(){
+
+}
+function fonteVer(){
+
+}
+function fonteNormal(){
+
+}
